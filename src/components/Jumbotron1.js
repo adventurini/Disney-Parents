9daac64a90f1b9320acc035cef9cbc7def2a2587
Anthony Venturini <anthonydventurini@gmail.com>
2019-06-15 21:38:00 -0700

    testing CDeployment

--- src/components/Jumbotron1.js
+++ src/components/Jumbotron1.js
@@ -11,8 +11,11 @@ class Jumbotron1 extends Component {
                 <h1 className="display-4">Theme Park Buddy</h1>
                 {this.props.loginForm ? 
                     <Button onClick={this.props.toggle} className="btn btn-lg btn-red">Sign Up</Button>
+                    <h2>Welcome, Please Sign Up</h2>
                         :
-                    <Button onClick={this.props.toggle} className="btn btn-lg btn-red">Sign In</Button>}
+                    <Button onClick={this.props.toggle} className="btn btn-lg btn-red">Sign In</Button>
+                    <h2>Welcome, Please Sign In</h2>}
+                    
             </Jumbotron>
             </ div>
             
