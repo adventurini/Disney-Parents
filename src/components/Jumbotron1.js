6604b3c205a66878104baf114e2ea7c7820e7dc6
Anthony Venturini <anthonydventurini@gmail.com>
2019-06-25 21:52:28 -0700

    updates styling

--- src/components/Jumbotron1.js
+++ src/components/Jumbotron1.js
@@ -1,6 +1,7 @@
 import React, {Component} from '../../node_modules/react';
 import './jumbotron.css';
 import {Button, Jumbotron} from '../../node_modules/reactstrap/lib';
+import image from '../assets/SeekPng.png'
 
 class Jumbotron1 extends Component {
     render(){
@@ -8,7 +9,8 @@ class Jumbotron1 extends Component {
             
             <div className="jumbotron">
             <Jumbotron>
-                <h1 className="display-4">Theme Park Buddy</h1>
+                <img src={image} alt="Walt Disney"></img>
+                <h1 className="display-4">Welcome to Disney Parents</h1>
                 {this.props.loginForm ? 
                     <Button onClick={this.props.toggle} className="btn btn-lg btn-red">Sign Up</Button>
                         :
